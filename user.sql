697984d0dab52a635c431ef0cf936f897521f623
jiangsongyu <245968148@qq.com>
2017-10-09 20:38:38 +0800

    100%

---
+++ user.sql
@@ -0,0 +1,7 @@
+INSERT INTO `user` (`username`, `password`) VALUES ('$username', '$password');
+INSERT INTO `user` (`username`, `password`) VALUES ('12321', '8ad3fac6c6b3528499d347d924443abb');
+INSERT INTO `user` (`username`, `password`) VALUES ('123123', '4297f44b13955235245b2497399d7a93');
+INSERT INTO `user` (`username`, `password`) VALUES ('jiangsongyu', '4297f44b13955235245b2497399d7a93');
+INSERT INTO `user` (`username`, `password`) VALUES ('jiangsongyu1', '4297f44b13955235245b2497399d7a93');
+INSERT INTO `user` (`username`, `password`) VALUES ('qwer', '4297f44b13955235245b2497399d7a93');
+INSERT INTO `user` (`username`, `password`) VALUES ('1234', '4297f44b13955235245b2497399d7a93');
